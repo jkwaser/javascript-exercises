cee15730a695cba072f4b54c76aeb458ffdaf234
jkwaser <jkwaser@gmail.com>
2022-03-16 22:29:01 +1000

    Completed exercise 2, passed all 7 tests and the 1 test suite.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, timesRepeat) {
+    string = "";
+    if (timesRepeat < 0){
+        return "ERROR"
+    }
+    for(let i = 0; i < timesRepeat; i++){
+        string += inputString;
+        console.log(string);
+    }
+    return string;
 };
 
 // Do not edit below this line
