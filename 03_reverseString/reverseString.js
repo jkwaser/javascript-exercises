8f91d3f7348373e657cd7897f7b1e7fdefd701a2
jkwaser <jkwaser@gmail.com>
2022-03-16 22:35:43 +1000

    Version of working reverse string function, passes all tests.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(stringInput) {
+    var newString = "";
+    for (var i = stringInput.length - 1; i >=0; i--) {
+        newString += stringInput[i];
+        console.log(newString);
+    }
+    return newString;
 };
 
 // Do not edit below this line
