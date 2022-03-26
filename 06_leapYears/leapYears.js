2a9b65b6b0cf565ad15795b9a0f0f7b2afef8a05
jkwaser <jkwaser@gmail.com>
2022-03-26 22:53:29 +1000

    Completed all tests successfully.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
+const leapYears = function(theYear) {
+    if(theYear % 400 == 0) {
+        return true
+    }
 
-};
+    else if(theYear % 100 == 0) {
+        return false
+    }
+
+    else if(theYear % 4 == 0) {
+        return true
+    }
+
+    else return false;
+
+}
 
 // Do not edit below this line
 module.exports = leapYears;
