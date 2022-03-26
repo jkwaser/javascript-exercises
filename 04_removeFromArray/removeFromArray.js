9a5c676c5351ed5c434219f4f2798c4a395b186f
jkwaser <jkwaser@gmail.com>
2022-03-26 22:33:04 +1000

    Learned about strict equality in Javascript. Now works for all tests.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,7 +2,7 @@ const removeFromArray = function(entryArray, itemRemove) {
     console.log(arguments);
     for (var j = 0; j <arguments.length; j++) {
         for(var i = 0; i < entryArray.length; i++){ 
-            if (entryArray[i] == arguments[j]){
+            if (entryArray[i] === arguments[j]){
             entryArray.splice(i, 1);
             }
         }
