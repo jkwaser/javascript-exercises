e2fa66675a4ea0f065e7cae9e7958e9ea2793cf6
jkwaser <jkwaser@gmail.com>
2022-03-26 22:26:45 +1000

    Removing from array works with first six tests. Still need to fix typing issue.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(entryArray, itemRemove) {
+    console.log(arguments);
+    for (var j = 0; j <arguments.length; j++) {
+        for(var i = 0; i < entryArray.length; i++){ 
+            if (entryArray[i] == arguments[j]){
+            entryArray.splice(i, 1);
+            }
+        }
+    }
+    return entryArray;
 };
 
 // Do not edit below this line
