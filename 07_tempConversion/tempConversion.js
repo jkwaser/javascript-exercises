4738215aae7b9fb86b37bfd15bb4771d514c54a6
jkwaser <jkwaser@gmail.com>
2022-03-26 22:59:00 +1000

    Completed exercise.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+  let celsius = (temperature - 32) * 5/9;
+  celsius = Math.round(celsius * 10) / 10;
+  return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  let farenheight = (temperature * 9/5) + 32;
+  farenheight = Math.round(farenheight * 10) / 10;
+  return farenheight;
 };
 
 // Do not edit below this line
