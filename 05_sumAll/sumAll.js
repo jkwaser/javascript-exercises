650ba46fd33bb1ad909c8eee956d1d440c68b0dd
jkwaser <jkwaser@gmail.com>
2022-03-26 22:46:28 +1000

    Completed exercise.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if (a <0 || b < 0) {
+        return "ERROR";
+    }
+    if (typeof a != 'number' || typeof b != 'number') {
+        return "ERROR";
+    }
+    let bigger = b;
+    let smaller = a;
+    let sum = 0;
+    if (a > b) {
+        bigger = a;
+        smaller = b;
+    }
+    
+    for(let i = smaller; i <= bigger; i++) {
+        sum += i;
+        console.log(sum);
+    }
+    return sum;
 
 };
 
