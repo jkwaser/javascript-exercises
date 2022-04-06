0a8a7d9f731e771c269c717e92e9f01af099df4b
jkwaser <jkwaser@gmail.com>
2022-04-06 22:43:24 +1000

    Completed all basic calculator functions.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -3,24 +3,37 @@ const add = function(a, b) {
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
   return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(toSum) {
+  initialSum = 0;
+  for (let i=0; i < toSum.length; i++) {
+    initialSum += toSum[i]
+    console.log(initialSum)
+  }
+  return initialSum;
 };
 
-const multiply = function() {
-
+const multiply = function(toMultiply) {
+  x = 1;
+  for (let i =0; i < toMultiply.length; i++) {
+    x = x * toMultiply[i];
+  }
+  return x;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let j = 1;
+	for (let i = a; i > 0; i--) {
+    j = j * i; 
+  }
+  return j;
 };
 
 // Do not edit below this line
