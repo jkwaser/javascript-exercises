96409cbf982798ea321d4859d36b8c4d9c880adf
jkwaser <jkwaser@gmail.com>
2022-04-11 23:31:51 +1000

    Made fibonacci function.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(fib) {
+    if (fib < 0) { return "OOPS" }
+    let a = 0;
+    let b = 1;
+    for (let i = 1; i < fib; i++) {
+        let x = b;
+        b = a + b;
+        a = x;
+    }
+    return b;
 };
 
 // Do not edit below this line
