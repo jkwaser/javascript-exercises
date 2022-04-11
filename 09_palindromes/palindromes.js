b3600f56aa9605b069c5a70a7a1ce63152f688e5
jkwaser <jkwaser@gmail.com>
2022-04-11 23:50:14 +1000

    Completed palindrome function. Probably too many functions in here and a random var from when I didn't understand how split worked in JS but it's done and it works.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (wordReverse) {
+  let wordLower = wordReverse.toLowerCase();
+  let wordTrue = wordLower.replace(/[^a-z]/g, "");
+  var wordArray = wordTrue.split("");
+  let reverseArray = wordArray.reverse();
+  let reverseWord = reverseArray.join("");
+  return reverseWord == wordTrue;
 };
 
 // Do not edit below this line
