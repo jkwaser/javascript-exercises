6dd3c49f5011b8f501c771a7035b6d735af29003
jkwaser <jkwaser@gmail.com>
2022-04-15 23:38:55 +1000

    Completed function, spent thirty minutes forgetting to capitalise the letter o.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(ageBracket) {
+    let oldest = 0;
+    let namae = {};
+    for (i in ageBracket) {
+        if (typeof ageBracket[i].yearOfDeath === 'undefined') {
+            const d = new Date();
+            ageBracket[i].yearOfDeath = d.getFullYear()        }
+    }
+    for (i in ageBracket) {
+        let TotalAge = ageBracket[i].yearOfDeath - ageBracket[i].yearOfBirth;
+        console.log(ageBracket[i])
+        console.log(TotalAge)
+        if (TotalAge > oldest) {
+        console.log(ageBracket[i])
+        oldest = ageBracket[i].yearOfDeath - ageBracket[i].yearOfBirth;
+        namae = ageBracket[i];
+        }
+    }
+    return namae
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
